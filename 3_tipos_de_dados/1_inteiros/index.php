d2fcbc8742367ebc1ae9c6e60d893e933d900e04
Rafael Casa <rafakroz@gmail.com>
2024-03-24 21:21:22 -0300

    Criando Seção 3 e adicionando arquivos

---
+++ 3_tipos_de_dados/1_inteiros/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo 5;
+    echo "<br>";
+    echo 5 + 7;
+    echo "<br>";
+    echo -12;
+    echo "<br>";
+    echo 5 - 12;
+    echo "<br><br>";
---
+++ 3_tipos_de_dados/2_S3_exercicio_3/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$n1 = 10;
+$n2 = 2;
+
+echo "Soma: $n1 + $n2 = ";
+echo $n1 + $n2;
+echo "<br>";
+
+echo "Subtração: $n1 - $n2 = ";
+echo $n1 - $n2;
+echo "<br>";
+
+echo "Multiplicação: $n1 * $n2 = ";
+echo $n1 * $n2;
+echo "<br>";
+
+echo "Divisão: $n1 / $n2 = ";
+echo $n1 / $n2;
+echo "<br>";
