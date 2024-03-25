8ab17e5dd8d519c1ad0cbaa0e478ea3972f8f6c1
Rafael Trabalho <rafakroz@gmail.com>
2024-03-25 08:32:06 -0300

    Adicionando as pastas 3 e 4

---
+++ 3_tipos_de_dados/3_checando_inteiro/index.php
@@ -0,0 +1,5 @@
+<?php
+
+    if(is_int(5)) {
+        echo "O número 5 é um inteiro";
+    }
---
+++ 3_tipos_de_dados/4_float/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // float
+
+    $n = 1.12;
+
+    echo $n;
+    echo "<br>";
+
+    echo 1.123;
+    echo "<br>";
+
+    echo 12.5 + 1.1;
+    echo "<br>";
