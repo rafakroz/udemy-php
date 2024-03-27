dc8eac9faf4025da3238e3338a2a1c5a1d94263a
Rafael Trabalho <rafakroz@gmail.com>
2024-03-27 12:21:21 -0300

    Adicionando as pastas 12 e 13 da seção 3

---
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a = [1, 2, 3];
+
+    print_r($a);
+    echo "<br>";
+    echo "<br>";
+    echo $a[0];
+    echo "<br>";
+
+    $arr = ["MAtheus", 1005, true];
+
+    echo "<br>";
+    
+    print_r($arr);
+    echo "<br>";
+    echo "<br>";
+
+    print_r($arr[1]);
+    
---
+++ 3_tipos_de_dados/13_array_associativo/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $arr = ['nome' => "Shark", 'patas' => 4, 'cor' => true];
+
+    echo $arr['nome'];
+    echo "<br>";
+    echo "<br>";
+    
+    print_r($arr);
+    echo "<br>";
+    echo "<br>";
+    
+    echo $arr['patas'];
+    echo "<br>";
+    echo "<br>";
+
+    $arrAssoc = ['chave' => "valor", 'bool' => true];
+
+    print_r($arrAssoc);
