ab62c0bf0a1de60ce280c14874027a7debef3617
Rafael Casa <rafakroz@gmail.com>
2024-03-31 18:23:11 -0300

    Seção 4 criada. Pastas 1 e 2 adicionadas

---
+++ 4_variaveis/1_criando_var/index.php
@@ -0,0 +1,39 @@
+<?php
+
+    $teste = "Algum texto";
+
+    echo $teste;
+
+    echo "<br>";
+    echo "<br>";
+    
+    $num = 10;
+    
+    echo $num;
+
+    echo "<br>";
+    echo "<br>";
+    
+    $_nome = "Rafael";
+    
+    echo $_nome;
+    //é possível utilizar undeline
+
+    echo "<br>";
+    echo "<br>";
+    
+    $velocidadeMaxima = 100;
+    //é recomendado que use caixa alta, apenas nas palavras seguintes
+
+        echo "A velocidade máxima é de $velocidadeMaxima km/h.";
+
+    echo "<br>";
+    echo "<br>";
+
+    $velocidade_minima = 30;
+    //é possível usar underline, mas, não usar, poupa caracter
+    
+        echo "A velocidade mínima é de $velocidade_minima km/h.";
+
+    echo "<br>";
+    echo "<br>";
---
+++ 4_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,39 @@
+<?php
+
+    $nome = "Rafael";
+
+    $solteiro = true;
+
+    $qtdViolao = 2;
+
+    echo "Meu nome é $nome.";
+    echo "<br>";
+
+    if ($solteiro == true) {
+        echo "Sou solteiro!";
+    }
+
+    echo "<br>";
+
+    echo "Eu tenho $qtdViolao violões!";
+
+    echo "<br>";
+    echo "<br>";
+    echo "------------------------------------------";
+    echo "<br>";
+    echo "<br>";
+    
+    // Aula ----------------------
+    
+    $marca = "Ferrari";
+    $velocidade = 250;
+    $itens = ["Teto solar", "Motor 4.0", "Piloto automático"];
+    
+    echo "A marcado carro é: $marca";
+    echo "<br>";
+        
+    echo "A velocidade máxima é de: $velocidade";
+    echo "<br>";
+
+    print_r($itens);
+    echo "<br>";
