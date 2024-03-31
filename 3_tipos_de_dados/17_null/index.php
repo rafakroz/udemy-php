73de0e008ac7c352cd095121fbcb36254850c31a
Rafael Casa <rafakroz@gmail.com>
2024-03-31 17:40:40 -0300

    Adicionando a pasta 17 da seção 3

--- 3_tipos_de_dados/17_null/index.php
+++ 3_tipos_de_dados/17_null/index.php
@@ -3,6 +3,17 @@
     echo NULL;
 
     echo "<br>";
+    
+    $nome = null;
+    
+    if(is_null($nome)){
+        echo "O valor é nulo";
+    }
+    
     echo "<br>";
 
-    
+    $nome = "Rafael";
+
+    if(is_null($nome)){
+        echo "O valor é nulo";
+    }
