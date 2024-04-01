97c0ada04f363e42574881e2273e81add1a4d16c
Rafael Trabalho <rafakroz@gmail.com>
2024-04-01 15:31:49 -0300

    Adicionando as pastas 3 e 4 da seção 4

---
+++ 4_variaveis/3_exercicio_9/index.php
@@ -0,0 +1,15 @@
+<?php
+
+     $num1 = 10;
+     $num2 = 5;
+
+     $resultado = $num1 + $num2;
+
+     echo "O resultado da soma de $num1 + $num2 é: $resultado";
+
+     echo "<br>";
+     echo "<br>";
+
+     // --------------------------------------------------------
+
+     
---
+++ 4_variaveis/4_var_de_var/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $x = "nome"; //variável x com valor nome
+
+    echo "$x <br><br>";
+
+    $$x = "Rafael"; // variável x (nome), com valor Rafael
+
+    echo "$nome <br>";
