3726291450e1131617d9ea3672d25d1e5a7e4dc3
Rafael Trabalho <rafakroz@gmail.com>
2024-04-09 08:19:54 -0300

    Adicionando as pastas 9 e 10 da seção 5

---
+++ 5_op_e_expressoes/10_operador_concatenacao/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    //Operador de concatenação
+
+    echo "Testando a concatenação";
+    echo "<br>";
+
+    echo "Testando" . " a " . "concatenação" . "<br>";
+
+    $t = "Testando";
+    $c = "concatenação";
+
+    echo $t . " a " . $c . "<br><br>";
+
+    $marca = "Ferrari";
+    $motor = "3.0";
+    $vel_max = 200;
+
+    echo "O carro da " . $marca . " tem um motor " . $motor . " com a velocidade máxima de " . $vel_max . "km/h." . "<br>";
---
+++ 5_op_e_expressoes/9_operador_exponenciacao/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Exponenciação de um número
+
+    echo 5 ** 2 . "<br><br>";
+    echo 2 ** 10 . "<br><br>";
+    
+    $a = 7;
+    $b = 4;
+    
+    echo $a ** $b . "<br><br>";
+
+    $c = 12;
+
+    $d = $c ** $a;
+
+    echo $d;
