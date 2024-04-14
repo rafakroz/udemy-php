5d4a0d3cf030b99d5ac5621fd76dd667beb73458
Rafael Casa <rafakroz@gmail.com>
2024-04-13 22:58:45 -0300

    Adicionando a pasta 20 da seção 5

---
+++ 5_op_e_expressoes/20_menor_igual_menor/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    if (4 < 12){
+        echo "4 é menor que 12! <br><br>";
+    }
+
+    if (12 <= 12){
+        echo "12 é menor que 12! <br><br>";
+    }
+
+    $a = 5;
+    $b = 18;
+    $c = 0;
+
+    if ($a <= $b){
+        echo "A é menor ou igual a B! <br><br>";
+    }
+
+    if ($b <= $a){
+        echo "B é menor ou igul a A! <br><br>";
+    }
+
+    if ($b <= 42){
+        echo "B é menor ou igual a 42! <br><br>";
+    }
