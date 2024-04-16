71795e5d4fe39f5c49faa9aa15259f7486a2406c
Rafael Trabalho <rafakroz@gmail.com>
2024-04-16 07:21:18 -0300

    Index da pasta 24, da seção 5, atualizado

--- 5_op_e_expressoes/24_exercicio_18/index.php
+++ 5_op_e_expressoes/24_exercicio_18/index.php
@@ -5,26 +5,32 @@
 //12 < 5 OR "João" === "João"
 
 if (5 < 5 || "João" === "João"){
-    echo "Questão 1 é verdadeira! <br>";
+    echo "5 < 5 || João === João <br>";
+    echo "Questão 1 é verdadeira! <br><br>";
 }
 else{
-    echo "Questão 1 não é verdadeira! <br>";
+    echo "5 < 5 || João === João <br>";
+    echo "Questão 1 não é verdadeira! <br><br>";
 }
 
 // 1 > 5 OR 1
 
 if (1 > 5 || 1){
-    echo "Questão 2 é verdadeira! <br>";
+    echo "1 > 5 || 1 <br>";
+    echo "Questão 2 é verdadeira! <br><br>";
 }
 else {
-    echo "Questão 2 não é verdadeira! <br>";
+    echo "1 > 5 || 1 <br>";
+    echo "Questão 2 não é verdadeira! <br><br>";
 }
 
 // 20 === "20" AND 51 >= 31
 
 if (20 === "20" && 51 >= 31){
+    echo "20 === '20' && 51 >= 31 <br>";
     echo "Questão 3 é verdadeira! <br>";
 }
 else{
+    echo "20 === '20' && 51 >= 31 <br>";
     echo "Questão 3 não é verdadeira! <br>";
 }
