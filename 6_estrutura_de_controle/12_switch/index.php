9ae2d17447251096f26489a743ca490a1ec5d0bf
Rafael Casa <rafakroz@gmail.com>
2024-04-21 21:54:43 -0300

    Adicionando a pasta 12 da seção 6

---
+++ 6_estrutura_de_controle/12_switch/index.php
@@ -0,0 +1,64 @@
+<?php
+
+//Switch
+
+    $x = 2;
+    $y = 5;
+    $w = "Rafael";
+    $z = "Matheus";
+
+    switch ($x) {
+        case 0:
+            echo "X é igual a 0. <br><br>";
+            break;
+        case 1:
+            echo "X é igual a 1. <br><br>";
+            break;
+        case 2:
+            echo "X é igual a 2. <br><br>";
+            break;
+        default:
+            echo "X não é igual a nenhum dos valores! <br><br>";
+    }
+
+    switch ($y) {
+        case 0:
+            echo "Y é igual a 0. <br><br>";
+            break;
+        case 1:
+            echo "Y é igual a 1. <br><br>";
+            break;
+        case 2:
+            echo "Y é igual a 2. <br><br>";
+            break;
+        default:
+            echo "Y não é igual a nenhum dos valores! <br><br>";
+    }
+
+    switch ($w) {
+        case "João":
+            echo "O nome é João. <br><br>";
+            break;
+        case "José":
+            echo "O nome é José. <br><br>";
+            break;
+        case "Rafael":
+            echo "O nome é Rafael. <br><br>";
+            break;
+        default:
+            echo "Nome não localizado! <br><br>";
+    }
+
+    switch ($z) {
+        case "João":
+            echo "O nome é João. <br><br>";
+            break;
+        case "José":
+            echo "O nome é José. <br><br>";
+            break;
+        case "Rafael":
+            echo "O nome é Rafael. <br><br>";
+            break;
+        default:
+            echo "Nome não localizado! <br><br>";
+    }
