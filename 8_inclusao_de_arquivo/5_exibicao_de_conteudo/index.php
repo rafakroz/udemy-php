ad7e2b43ab347d88a95ffba9e1be97b93b81ed5b
Rafael Trabalho <rafakroz@gmail.com>
2024-05-02 08:32:36 -0300

    Adicionando a pasta 5 da seção 8

---
+++ 8_inclusao_de_arquivo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nome = "Rafael";
+    $sobrenome = "Queiroz";
+
+?>
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome; ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $sobrenome; ?>">
+    </div>
+    <br>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
