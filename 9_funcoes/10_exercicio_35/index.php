edff633b4488ec72c9db756041ad0955023103bf
Rafael Trabalho <rafakroz@gmail.com>
2024-05-06 12:55:45 -0300

    Adicionando a pasta 10 da seção 9

---
+++ 9_funcoes/10_exercicio_35/index.php
@@ -0,0 +1,17 @@
+<?php
+
+function quadrado ($numero) {
+
+    $resultado = $numero ** 2; 
+
+    return $resultado;
+
+}
+
+$n1 = 2;
+$n2 = 3;
+$n3 = 12;
+
+echo quadrado($n1) . "<br>";
+echo quadrado($n2) . "<br>";
+echo quadrado($n3) . "<br>";
