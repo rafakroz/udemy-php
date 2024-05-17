865b0714d6ae7b4f863f4c624b4f8b7812b15d50
Rafael Trabalho <rafakroz@gmail.com>
2024-05-17 16:37:05 -0300

    Criada a seção 12, e adicionadas as pastas 1 e 2

---
+++ 12_intro_orientacao_a_objetos/1_criando_classe/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+DECLARANDO UMA CLASSE
+
+• Para iniciar uma classe vamos precisar da palavra reservada class,
+e também dar um nome;
+
+• Por convenção, a inicial do nome é sempre maiúscula;
+
+• Como é um bloco de código, a clase é envolvida por {}:
+
+    Exemplo: 
+        class User {
+
+        }
+
+*/
+
+class User {
+
+}
+
+class Programador {
+
+    // propriedades
+    // método
+
+}
---
+++ 12_intro_orientacao_a_objetos/2_instanciando_objeto/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+INSTANCIANDO OBJETO
+
+• Para instanciar um objeto vamos utilizar a palavra new em conjunto do nome da classe;
+
+• A partir daí, uma entidade com as características da classe será criada;
+
+• Normalmente encapsulamos este valor em uma variável:
+
+    Exemplo: 
+    $rafael = new User;
+*/
+
+class User {
+
+}
+
+$rafael = new User;
+$fred = new User;
+$ester = new User;
+
+//Uma mesma classe com propriedade e método que podem ser instanciadas para diversos objetos.
+
+//Os vários usuários fariam uso de uma mesma estrutura da classe, no entanto, com seus próprios valores.
