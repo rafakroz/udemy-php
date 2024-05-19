616fe38b1debd213023f429c15cf17070296092c
Rafael Casa <rafakroz@gmail.com>
2024-05-19 20:30:59 -0300

    Adicionando a pasta 8 da seção 12

---
+++ 12_intro_orientacao_a_objetos/8_this/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+CONHECENDO O $THIS
+
+• o $this (tradução literal: este) se refere a instância atual do objeto;
+
+• Podendo assim alterar um valor de uma propriedade do objeto com:
+
+    $this -> propriedade = "x";
+
+• Podemos invocar um método do objeto com this também.
+
+*/
+
+class Animal {
+
+    public $nome;
+
+    function escolherNome($nome) {
+
+        $this->nome = $nome;
+
+    }
+
+    function latir() {
+
+        return "Au au! <br>";
+
+    }
+
+    function latirForte() {
+
+        return strtoupper($this->latir());
+
+    }
+
+}
+
+$frida = new Animal;
+
+echo "O nome do animal é: $frida->nome <br><br>";
+
+// O nome não será impresso, pois não foi definido.
+
+$frida->escolherNome("Frida");
+
+// Nome definido. Será impresso normalmente
+
+echo "O nome do animal é: $frida->nome <br><br>";
+
+echo "A $frida->nome latiu: " . $frida->latir();
+
+echo "A $frida->nome latiu forte: " . $frida->latirForte();
