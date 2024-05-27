abac694fe6a254e5d3840ab43bb96f8916a3628d
Rafael Casa <rafakroz@gmail.com>
2024-05-26 21:56:27 -0300

    Página post finalizada

--- 15_projeto_1_blog/post.php
+++ 15_projeto_1_blog/post.php
@@ -24,9 +24,9 @@
 
         <div class="content-container">
             <h1 id="main-title"><?= $currentPost['title'] ?></h1>
-            <p class="post-description"><?= $currentPost['description'] ?></p>
+            <p id="post-description"><?= $currentPost['description'] ?></p>
 
-            <div class="omg-containet">
+            <div class="img-container">
                 <img src="<?= $BASE_URL ?>img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
             </div>
 
@@ -43,29 +43,31 @@
 
         </div>
 
+        <aside id="nav-container">
+
+            <h3 id="tags-title">Tags</h3>
+            <ul id="tag-list">
+                <?php foreach($currentPost['tags'] as $tag) : ?>
+                    <li>
+                        <a href="#"><?= $tag ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
+            <h3 id="categories-title">Categorias</h3>
+            <ul id="categories-list">
+                <?php foreach($categories as $category) : ?>
+                    <li>
+                        <a href="#"><?= $category ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
+        </aside>
+
     </main>
 
-    <aside id="nav-container">
 
-        <h3 id="tag-list">Tags</h3>
-        <ul class="tags-container">
-            <?php foreach($currentPost['tags'] as $tag) : ?>
-                <li>
-                <a href="#"><?= $tag ?></a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-
-        <h3 id="categories-title">Categoria</h3>
-        <ul class="categories-list">
-            <?php foreach($categories as $category) : ?>
-                <li>
-                <a href="#"><?= $category ?></a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-
-    </aside>
 
 <?php
 
