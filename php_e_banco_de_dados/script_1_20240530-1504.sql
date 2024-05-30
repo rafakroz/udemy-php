25561799f65cf3db2a966090dfcc3f836dcf5db5
Rafael Casa <rafakroz@gmail.com>
2024-05-30 16:23:02 -0300

    Scrip atualizado até a aula 294

--- php_e_banco_de_dados/script_1_20240530-1504.sql
+++ php_e_banco_de_dados/script_1_20240530-1504.sql
@@ -16,14 +16,366 @@ create table fornecedores (
 	nome character varying(100)
 )
 
+	
+	
 -- 290
 -- DROP TABLE nome : apagar tabela
 
 DROP TABLE fornecedores
 
+
+	
 -- 291. Modificando tabelas
 -- ALTER TABLE nome
 -- ADD/DROP CLOUMN/MODIFY COLUMN nome
+
+ALTER TABLE carros
+ADD motor INT;
+
+ALTER TABLE carros
+DROP ano_fabricacao;
+
+ALTER TABLE carros
+ALTER COLUMN descricao TYPE CHARACTER Varying(500)
+
+
+
+-- 292. Sobre as CONSTRAINTS
+
+-- 293. CONSTRAINT NOT NULL
+-- nome VARCHAR(100) NOT NULL
+
+CREATE TABLE not_null (
+	nome varchar(100) NOT NULL,
+	idade INT
+)
+
+INSERT into not_null (nome, idade) VALUES ('Fred', 2)
+
+select * from not_null
+
+INSERT into not_null (nome, idade) values ('', 10)
+
+insert into not_null (idade) values (15)
+
+insert into not_null (nome) values ('Layla')
+
+
+
+-- 294. CONSTRAINT UNIQUE
+-- nome VARCHAR(100) UNIQUE
+-- email VARCHAR(255) NOT NULL UNIQUE
+
+create TABLE mailmarketing (
+	email varchar(255) not null unique,
+	nome varchar(255) 
+)
+
+select * from mailmarketing
+
+INSERT into mailmarketing (email, nome) values ('rafael@teste.com', 'Rafael')
+
+INSERT into mailmarketing (nome) values ('Rafael') -- necessita de email
+
+INSERT into mailmarketing (email) values ('rafael@teste.com') -- email já existente
+	
+INSERT into mailmarketing (email, nome) values ('rafael@teste.com', 'Fred') -- email já existente
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
