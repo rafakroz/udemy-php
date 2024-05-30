71bc0d732f558892ccc9893ad32320f93ba006aa
Rafael Casa <rafakroz@gmail.com>
2024-05-30 15:44:23 -0300

    Criada pasta da aula de SQL do curso

---
+++ php_e_banco_de_dados/script_1_20240530-1504.sql
@@ -0,0 +1,32 @@
+-- Curso UDEMY - PHP do Zero a Maestria + 4 Projetos incríveis
+
+CREATE TABLE pessoas (
+	nome character varying(200),
+	idade INT
+);
+
+create table carros (
+	marca character varying(100),
+	descricao text,
+	ano_fabricacao INT	
+)
+
+create table fornecedores (
+	id_forncedor int,
+	nome character varying(100)
+)
+
+-- 290
+-- DROP TABLE nome : apagar tabela
+
+DROP TABLE fornecedores
+
+-- 291. Modificando tabelas
+-- ALTER TABLE nome
+-- ADD/DROP CLOUMN/MODIFY COLUMN nome
+
+
+
+
+
+
