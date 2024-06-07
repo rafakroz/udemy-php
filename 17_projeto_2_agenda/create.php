243d67b125a1598f9864d286ee77f91279015719
Rafael-Trabalho <rafael.queiroz@controllab.com.br>
2024-06-07 10:49:34 -0300

    CSS do create não está funcionando

--- 17_projeto_2_agenda/create.php
+++ 17_projeto_2_agenda/create.php
@@ -1,13 +1,31 @@
 <?php
-
     include_once("templates/header.php");
-
 ?>
 
-    <h1>Criando Contato</h1>
+    <div class="container">
+        <?php include_once("templates/backbtn.html"); ?>
+
+        <h1 id="main-title">Criar contato</h1>
+
+        <form id="crea-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do Contato:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do Contato:</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" class="form-control" rows="3" id="observations" name="observations" placeholder="Insira as Observações" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+
+    </div>
 
 <?php
-
     include_once("templates/footer.php");
-
 ?>
