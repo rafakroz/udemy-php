85874831f2da3fbd7f0db2acd3e8fb63ae70771f
Rafael Queiroz <rafakroz@gmail.com>
2024-06-09 20:22:57 -0300

    Update implementado

--- 17_projeto_2_agenda/config/process.php
+++ 17_projeto_2_agenda/config/process.php
@@ -32,7 +32,38 @@
             try {
 
                 $stmt->execute();
-                $_SESSION['msg'] = "Contato criado com sucesso";
+                $_SESSION['msg'] = "Contato criado com sucesso!";
+
+            } catch (PDOException $e) {
+
+                // Erro na conexão
+                $error = $e->getMessage();
+                echo 'Erro: ' . $error;
+            }
+        }
+
+        else if ($data['type'] === 'edit') {
+
+            $name = $data['name'];
+            $phone = $data['phone'];
+            $observations = $data['observations'];
+            $id = $data['id'];
+
+            $query = 'UPDATE contacts
+                      SET name = :name, phone = :phone, observations = :observations
+                      WHERE id = :id';
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':phone', $phone);
+            $stmt->bindParam(':observations', $observations);
+            $stmt->bindParam(':id', $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION['msg'] = "Contato atualizado com sucesso!";
 
             } catch (PDOException $e) {
 
