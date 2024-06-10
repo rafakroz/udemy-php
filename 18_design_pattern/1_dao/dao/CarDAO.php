e8d34cffae94571f77911e4e1283be2aefd352a7
Rafael-Trabalho <rafael.queiroz@controllab.com.br>
2024-06-10 11:11:09 -0300

    Comentários inseridos

--- 18_design_pattern/1_dao/dao/CarDAO.php
+++ 18_design_pattern/1_dao/dao/CarDAO.php
@@ -2,9 +2,7 @@
 
     include_once("models/Car.php");
 
-/**
- * Implementando a interface criada em Car.php
- */
+// Implementando a interface criada em Car.php
 class CarDAO implements CarDAOInterface {
 
     private $conn;
@@ -13,16 +11,22 @@ class CarDAO implements CarDAOInterface {
         $this->conn = $conn;
     }
 
+    // Select para todos os itens do banco
     public function findAll() {
 
+        // será retornado um array com todos os dados
         $cars = [];
 
+        // Como se trata de select *, não precisa de prepare e bindParam / bindValues
         $stmt = $this->conn->query('SELECT * FROM cars');
 
+        // Obter todos os dados da query
         $data = $stmt->fetchAll();
 
+        // Foreach com os dados que vieram da query
         foreach ($data as $item) {
 
+            // Criando o objeto para adicionar no array
             $car = new Car();
 
             $car->setId($item['id']);
@@ -30,13 +34,15 @@ class CarDAO implements CarDAOInterface {
             $car->setKm($item['km']);
             $car->setColor($item['color']);
 
+            // Inserindo o objeto no array
+            // Como está sem índice, será inserido no final
             $cars[] = $car;
         }
-
+        // Retorna para a view
         return $cars;
-
     }
 
+    // Inserção de novo item
     public function create(Car $car) {
 
         $stmt = $this->conn->prepare('INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)');
@@ -46,7 +52,5 @@ class CarDAO implements CarDAOInterface {
         $stmt->bindValue(':color', $car->getColor());
 
         $stmt->execute();
-
     }
-
 }
--- 18_design_pattern/1_dao/process.php
+++ 18_design_pattern/1_dao/process.php
@@ -1,10 +1,15 @@
 <?php
 
+// Efetuar a conexão com o banco
 include_once("db.php");
+
+// Manipulação de dados
 include_once("dao/CarDAO.php");
 
+// Criando uma classe baseada no DAO
 $carDao = new CarDao($conn);
 
+// Inputs/Dados que virão do post
 $brand = $_POST['brand'];
 $km = $_POST['km'];
 $color = $_POST['color'];
