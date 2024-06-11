8b293ec9494b20352e96f1b3ee5b1a454aa91027
Rafael Queiroz <rafakroz@gmail.com>
2024-06-10 21:23:24 -0300

    Separação dos templates efetuada

---
+++ 19_projeto_3_moviestar/templates/footer.php
@@ -0,0 +1,36 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li>
+                    <a href="#">Adicionar filme</a>
+                </li>
+                <li>
+                    <a href="#">Adicionar review</a>
+                </li>
+                <li>
+                    <a href="#">Entrar / Registrar</a>
+                </li>
+            </ul>
+        </div>
+        <p>&copy; 2024 Moviestar Inc.</p>
+    </footer>
+
+<!-- BOOTSTRAP JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
