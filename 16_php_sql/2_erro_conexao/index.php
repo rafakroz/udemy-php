25772e053c0792a4ed0fe28f2b457a1d2bfaa874
Rafael Queiroz <rafakroz@gmail.com>
2024-06-15 23:10:55 -0300

    Arquivo retificado

--- 16_php_sql/2_erro_conexao/index.php
+++ 16_php_sql/2_erro_conexao/index.php
@@ -7,13 +7,11 @@ $db = "cursophp";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-<<<<<<< HEAD
 if($conn->connect_errno) {
     echo "Erro na conexão!" . "<br>";
-=======
+}
 
 if($conn->connect_errno) {
     echo "Erro na conexão!" . "<br>";
     echo "Erro: " . $conn->connect_error;
->>>>>>> e5a20b45611ee28a739847dba7ffeb0b30ac2763
 }
