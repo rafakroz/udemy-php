ffb042ea53aa3ff37e0edba264e7eca23bbf055b
Rafael Casa <rafakroz@gmail.com>
2024-06-15 22:54:44 -0300

    Header logado e LogOut implementados

---
+++ 16_php_sql/2_erro_conexao/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "cursophp";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn->connect_errno) {
+    echo "Erro na conexão!" . "<br>";
+}
